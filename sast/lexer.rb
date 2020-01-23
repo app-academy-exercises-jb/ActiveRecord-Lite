55ade51a32cb77bd207fa6c88b64b32a73df31be
Jorge Barreto <jrbd93@gmail.com>
2020-01-22 20:14:16 -0500

    fixed operator precedence parsing

--- sast/lexer.rb
+++ sast/lexer.rb
@@ -1,83 +1,96 @@
-module Lexer
-  QUERIES = %w(select from join where limit)
-  MODIFIERS = %w(distinct as)
-  OPERATORS = %w(on between like in and or)
+class Sast
+  module Lexer
+    QUERIES = %w(select from join where limit)
+    MODIFIERS = %w(distinct as)
+    OPERATORS = %w(on between like in and or)
 
-  def self.tokenize(query)
-    tokens = []
+    def self.tokenize(query)
+      tokens = []
 
-    current_token = ""
-    current_token_type = :word
-
-    change_token = -> (token, type) { 
-      if QUERIES.include?(token.downcase) 
-        type = :reserved 
-      elsif MODIFIERS.include?(token.downcase) 
-        type = :modifier
-      elsif OPERATORS.include?(token.downcase)
-        type = :operator
-      end
-      tokens << Token.new(type, token) unless token == ""
       current_token = ""
-    }
+      current_token_type = :word
 
-    inter_token = -> (token,type) {
-      # periods and commas may sometimes be attached to a word
-      change_token.call(current_token[0..-2], current_token_type)
-      current_token_type = type
-      change_token.call(token, current_token_type)
-    }
+      change_token = -> (token, type) { 
+        if QUERIES.include?(token.downcase) 
+          type = :reserved 
+        elsif MODIFIERS.include?(token.downcase) 
+          type = :modifier
+        elsif OPERATORS.include?(token.downcase)
+          type = :operator
+        elsif type == :nil
+          type = :value
+        end
+        token = token.downcase unless type == :value || type == :word
+        # debugger
+        tokens << Token.new(type, token) unless token == ""
+        current_token = ""
+      }
 
-    query.each_char.with_index { |chr,idx| 
-      current_token += chr unless /\s/ === chr
+      inter_token = -> (token,type) {
+        # periods and commas may sometimes be attached to a word
+        change_token.call(current_token[0..-2], current_token_type)
+        current_token_type = type
+        change_token.call(token, current_token_type)
+      }
 
-      if /(\w|\.|\*)/ === chr
-        # we are in the middle of(inclusive) a word
-        current_token_type = :word
-      elsif /'/ === chr
-        # we're starting or stopping reading a value
-        current_token_type = :value
-      elsif /(>=|<=|<|>|=)/ === chr
-        # we found an operator token
-        current_token_type = :operator
-      # elsif /\*/ === chr
-      #   # we found a wildcard token
-      #   # wildcard tokens are specific types of column names
-      #   current_token_type = :wildcard
-      elsif /,/ === chr
-        # we found a comma token
-        inter_token.call(",", :comma)
-      # elsif /\./ === chr
-      #   # we found a period token
-      #   inter_token.call(".", :period)
-      elsif /\(/ === chr || /\)/ === chr
-        inter_token.call(chr, :paren)
-      elsif /\)/ === chr
-        # current_token_type = :paren
-        # change_token.call(current_token,current_token_type)
-      elsif /\s/ === chr
-        # we finished reading a token
-        change_token.call(current_token, current_token_type)
-      else
-        raise "fatal. unrecognized character #{chr}"
+      query.each_char.with_index { |chr,idx| 
+        if current_token_type == :value 
+          current_token += chr
+        else
+          unless /\s/ === chr 
+            current_token += chr
+          end
+        end
+
+
+        if /(\w|\.|\*)/ === chr
+          # we are in the middle of(inclusive) a word
+          # but preserve :value type
+          current_token_type = :word unless current_token_type == :value
+        elsif /'/ === chr
+          # we're starting or stopping reading a value
+          if current_token_type == :value
+            current_token_type = :nil
+          else
+            current_token_type = :value
+          end
+        elsif /(>=|<=|<|>|=|\+|\*|\/|-)/ === chr
+          # we found an operator token
+          current_token_type = :operator
+        elsif /,/ === chr
+          inter_token.call(",", :comma)
+        elsif /\(/ === chr || /\)/ === chr
+          inter_token.call(chr, :paren)
+        elsif /\)/ === chr
+          # current_token_type = :paren
+          # change_token.call(current_token,current_token_type)
+        elsif /\s/ === chr
+          # we finished reading a token, unless we're in the middle of a value literal
+
+          change_token.call(current_token, current_token_type) unless current_token_type == :value
+        else
+          raise "fatal. unrecognized character #{chr}"
+        end
+      }
+
+      change_token.call(current_token, current_token_type) unless current_token.empty?
+      tokens
+    end
+
+    class Token
+      attr_reader :type, :value
+      def initialize(type, value)
+        @type, @value = type, value
       end
-    }
+      def to_s
+        value
+      end
+      def ==(val)
+        value == val
+      end
+    end
 
-    change_token.call(current_token, current_token_type) unless current_token.empty?
-    tokens
-  end
-end
-
-class Token
-  attr_reader :type, :value
-  def initialize(type, value)
-    @type, @value = type, value
-  end
-  def to_s
-    value
-  end
-  def ==(val)
-    value == val
+    const_set :NullToken, Token.new(:null, nil)
   end
 end
 
--- sast/parser.rb
+++ sast/parser.rb
@@ -16,140 +16,223 @@
 # We observe the syntactic rules set out here: https://www.sqlite.org/lang_select.html
 
 require "byebug"
-module Parser
-  def self.generate_tree(tokens)
-    count = -1
-    current_token = nil;
-
-    return_options = ->(options,type,value) {
-      options.empty? ?
-        SastNode.new(type: type, value: value) :
-        SastNode.new(type: type, value: value, options: options)
+class Sast
+  module Parser
+    PRECEDENCE = {
+      nil => -1, 
+      "on" => 0,
+      "or" => 0,
+      "and" => 1,
+      "like" => 2,
+      "in" => 2,
+      "!=" => 2,
+      "=" => 2,
+      ">=" => 3,
+      ">" => 3,
+      "<=" => 3,
+      "<" => 3,
+      "-" => 4,
+      "+" => 4,
+      "/" => 5,
+      "*" => 5
     }
-    gather_values = ->(tokens, prc) {
-      values = []
-      current_val = [tokens[0]]
-      tokens[1..-1].each { |token|
-        prc.call(token) ?
-          (values << current_val; current_val = [token]) :
-          current_val << token
+    def self.generate_tree(tokens)
+      count = -1
+      current_token = nil;
+
+      return_options = ->(options,type,value) {
+        options.empty? ?
+          SastNode.new(type: type, value: value) :
+          SastNode.new(type: type, value: value, options: options)
       }
-      values << current_val
-      values.map! { |val| self.generate_tree(val) }
-    }
-    counter = ->() { count += 1; current_token = tokens[count] }
-    walk = ->() {
-      counter.call
-      options = Hash.new { |h,k| h[k.type] = k}
 
-      case current_token.type
-      when :reserved
-        node_tokens = []
-        type = current_token.value.downcase.to_sym
+      gather_values = ->(tokens, prc) {
+        values = []
+        current_val = [tokens[0]]
+        tokens[1..-1].each { |token|
+          prc.call(token) ?
+            (values << current_val; current_val = [token]) :
+            current_val << token
+        }
+        values << current_val
+        values.map! { |val| self.generate_tree(val) }
+      }
+
+      counter = ->() { count += 1; current_token = tokens[count] }
+
+      comp_ops = ->(opers, opans, tok) {
+        if PRECEDENCE[tok.value] > PRECEDENCE[opers[-1].value]
+          opers << tok
+        else
+          # raise "fatal: binary operation with more than two operands" unless opans.length == 2
+          new_operand = self.generate_tree([opers.pop, opans.pop, opans.pop])
+          opans << new_operand
+          opers << tok
+        end
+      }
+
+      parse_operators = ->(tokens) {
+        operators = [Lexer::NullToken]
+        operands = []
+
+        paren_count = 0
+        subquery = []
         
-        counter.call
-        until current_token == nil || (current_token.type == :reserved && current_token.value != "join")
-          node_tokens << current_token
-          counter.call
+
+        tokens.each { |tok| 
+          if paren_count > 0
+            subquery << tok
+            if tok.value == ")"
+              paren_count -= 1
+              operands << self.generate_tree(subquery) if paren_count == 0
+            elsif tok.value == "("
+              paren_count += 1
+            end
+          else
+            if tok.type == :operator
+              comp_ops.call(operators, operands, tok)
+            elsif tok.value == "("
+              paren_count += 1
+              subquery << tok
+            else
+              operands << tok
+            end
+          end
+        }
+        
+
+        until operators.length == 2
+          # operands << comp_ops.call(operators, operands, operators.pop)
+          new_operand = self.generate_tree([operators.pop, operands.pop, operands.pop])
+          operands << new_operand
         end
 
-        # we've detected a subquery
-        if node_tokens[-1].value == "("
-          until current_token.value == ")"
+        self.generate_tree([operators.pop, operands.pop, operands.pop])
+
+        # raise "fatal: did not resolve" unless operands.length == 1
+        # operands[0]
+      }
+
+      walk = ->() {
+        counter.call
+        options = Hash.new { |h,k| h[k.type] = k}
+
+        case current_token.type
+        when :reserved
+          node_tokens = []
+          type = current_token.value.downcase.to_sym
+          
+          counter.call
+          until current_token == nil || (current_token.type == :reserved && current_token.value != "join")
             node_tokens << current_token
             counter.call
           end
-          node_tokens << current_token
-          2.times { counter.call; node_tokens << current_token} #catch the name
-        end
-        
-        # we've got a SELECT, which may have a DISTINCT, options, and several selected values
-        if type == :select
-          raise SyntaxError.new("expecting values for SELECT statement") if node_tokens.empty?
 
-          value = node_tokens[0].value == "distinct" ?
-            (options[:distinct] = true; self.generate_tree(node_tokens[1..-1])) :
-            gather_values.call(node_tokens, ->(t) { t.type == :comma })
-
-          until count >= tokens.length - 1
-            count -= 1
-            options[walk.call]
+          # we've detected a subquery
+          if node_tokens[-1].value == "("
+            until current_token.value == ")"
+              node_tokens << current_token
+              counter.call
+            end
+            node_tokens << current_token
+            # FROM + JOIN must name subqueries
+            # WHERE need not
+            2.times { counter.call; node_tokens << current_token} unless type == :where #catch the name
+            raise SyntaxError.new("subquery must be named") if node_tokens[-1].nil?
           end
-        end
-        
-        # we've got a WHERE which has a series of operators and operands 
-        if type == :where
-          #value = gather_values.call(node_tokens, ->(t) { t.type == :modifier && t.value != "as" })
-          # here we must implement an operator-precedence parser
-          value = self.generate_tree(node_tokens)
-        end
+          
+          # we've got a SELECT, which may have a DISTINCT, options, and several selected values
+          if type == :select
+            raise SyntaxError.new("expecting values for SELECT statement") if node_tokens.empty?
 
-        # we've got a FROM, which may have multiple tables, subqueries, and JOINs
-        if type == :from
-          tables = []
-          joins = []
-          has_joins = false
-          # separate our tables and joins
-          node_tokens.each { |token|
-            has_joins = true if !has_joins && token.value == "join"
-            has_joins ? 
-              joins << token :
-              tables << token
-          }
+            value = node_tokens[0].value == "distinct" ?
+              (options[:distinct] = true; self.generate_tree(node_tokens[1..-1])) :
+              gather_values.call(node_tokens, ->(t) { t.type == :comma })
 
-          if joins.any?
-            options[:join] = gather_values.call(joins, ->(j) { j.type == :reserved })
+            until count >= tokens.length - 1
+              count -= 1
+              options[walk.call]
+            end
+          end
+          
+          # we've got a WHERE which has a series of operators and operands 
+          if type == :where
+            #value = gather_values.call(node_tokens, ->(t) { t.type == :modifier && t.value != "as" })
+            value = self.generate_tree(node_tokens)
           end
 
-          raise SyntaxError.new("must select FROM a table or subquery") if tables.empty?
+          # we've got a FROM, which may have multiple tables, subqueries, and JOINs
+          if type == :from
+            tables = []
+            joins = []
+            has_joins = false
+            # separate our tables and joins
+            node_tokens.each { |token|
+              has_joins = true if !has_joins && token.value == "join"
+              has_joins ? 
+                joins << token :
+                tables << token
+            }
+
+            if joins.any?
+              options[:join] = gather_values.call(joins, ->(j) { j.type == :reserved })
+            end
+
+            
+
+            raise SyntaxError.new("must select FROM a table or subquery") if tables.empty?
+            
+            value = gather_values.call(tables, ->(t) { t.type == :comma })
+          end
           
-          value = gather_values.call(tables, ->(t) { t.type == :comma })
-        end
-        
-        # we've found a JOIN, which may name a table or subquery and expects an operator ON
-        if type == :join
-          value = self.generate_tree(node_tokens)
-        end
+          # we've found a JOIN, which may name a table or subquery and expects an operator ON
+          if type == :join
+            value = self.generate_tree(node_tokens)
+          end
 
-        # we've found a LIMIT
-        if type == :limit
-          value = self.generate_tree(node_tokens)
-        end
-        
-        return_options.call(options,type,value)
-      when :paren
-        # we're in a subquery. collect the tokens till we find the closing paren, and call ::generate_tree on that
-        subquery_tokens = [current_token]
-        options = {}
-
-        until current_token.type == :paren && current_token.value == ")"
-          counter.call
-          subquery_tokens << current_token
-        end
-
-        # the subquery may be named:
-        if tokens[count+1]&.type == :modifier
-          options = {alias: self.generate_tree([tokens[count+2]]) }
-        end        
-
-        value = self.generate_tree(subquery_tokens[1..-2])
-        return_options.call(options,:query,value)
-      when :word
-        if tokens[count+1]&.type == :operator
-          # we have to define a hierarchy for operator precedence
-          name = self.generate_tree([tokens[count]])
-          value = self.generate_tree(tokens[count+2..-1])
+          # we've found a LIMIT
+          if type == :limit
+            value = self.generate_tree(node_tokens)
+          end
           
-          SastNode.new(type: :operator, value: [name, value], options: {operator: tokens[count+1].value})
-        else
-          SastNode.new(type: :name, value: current_token.value)
+          return_options.call(options,type,value)
+        when :paren
+          # we're in a subquery. collect the tokens till we find the closing paren, and call ::generate_tree on that
+          subquery_tokens = [current_token]
+          options = {}
+
+          until current_token.type == :paren && current_token.value == ")"
+            counter.call
+            subquery_tokens << current_token
+          end
+
+          # the subquery may be named:
+          if tokens[count+1]&.type == :modifier
+            options = {alias: self.generate_tree([tokens[count+2]]) }
+          end        
+
+          value = self.generate_tree(subquery_tokens[1..-2])
+          return_options.call(options,:query,value)
+        when :word, :value
+          if tokens[count+1]&.type == :operator
+            
+            parse_operators.call(tokens)
+          else
+            type = current_token.type == :word ? :name : :value
+            SastNode.new(type: type, value: current_token.value)
+          end
+        when :comma, :modifier
+          self.generate_tree(tokens[1..-1])
+        when :operator
+          # we expect to have two tokens.
+          # tokens[0], tokens[1]
+          left = tokens[1].is_a?(SastNode) ? tokens[1] : self.generate_tree([tokens[1]])
+          right = tokens[2].is_a?(SastNode) ? tokens[2] :  self.generate_tree([tokens[2]])
+          SastNode.new(type: :operator, value: [right, left], options: {operator: current_token.value})
         end
-      when :value
-        SastNode.new(type: :value, value: current_token.value)
-      when :comma, :modifier
-        self.generate_tree(tokens[1..-1])
-      end
-    }
-    walk.call
+      }
+      
+      walk.call
+    end
   end
 end
